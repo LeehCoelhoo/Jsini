2be4d193a9282abca7d50025e7975dc455cbb1f4
Letícia <leehcoelhoo@gmail.com>
2020-08-18 19:09:03 +0000

    Adição de novas variaveis

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ console.log(numero); // typeof(numero)
 console.log(typeof(numero));
 var palavra = "eu to com fome"; // Tipo string
 console.log(palavra);
+console.log(typeof(palavra));
 var logica =  false; //Tipo boolean;
 console.log(logica);
+console.log(typeof(logica));
+var qualquer;
+console.log(qualquer);
+console.log(typeof(qualquer));
 
