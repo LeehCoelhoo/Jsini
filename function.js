9b3ebc0e92c4fb630e33e26ec3b9f3c29c366ef7
Letícia <leehcoelhoo@gmail.com>
2020-08-26 20:24:56 +0000

    modules.modulo

---
+++ function.js
@@ -0,0 +1,29 @@
+// Aula 3
+/*
+function <nome da função> (parametro)
+{
+
+}
+*/
+
+/*
+function somar(){
+  var x,y;
+  x=10;
+  y=10;
+  var soma = x+y;
+  console.log(soma);
+}
+somar()
+*/
+
+const somar = function (x,y){ 
+  var soma = x+y;
+  return soma;
+  }
+
+  var z= somar(4,6);
+  console.log(z);
+
+
+
--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-var numero = 1; //  Tipo number (1,10,2.5,5.3450)
-console.log(numero); // typeof(numero)
-console.log(typeof(numero));
-var palavra = "eu to com fome"; // Tipo string
-console.log(palavra);
-console.log(typeof(palavra));
-var logica =  false; //Tipo boolean;
-console.log(logica);
-console.log(typeof(logica));
-var qualquer;
-console.log(qualquer);
-console.log(typeof(qualquer));
-
+var modulo = require ('./modules/modulos.js');
+modulo.modulo();
